eb94b6d763831206998e40966aa87c7438da7998
jpalenceloph-collab <janriepalenzuela.celoph@gmail.com>
2026-09-25 17:06:27 -0500

    Update Gladly-SLA-Tracker.user.js

--- Gladly-SLA-Tracker.user.js
+++ Gladly-SLA-Tracker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gladly SLA Tracker + Liveboard Monitor
 // @namespace    gladly-sla-tracker
-// @version      2.8.4
+// @version      2.8.5
 // @description  ...
 // @match        https://*.gladly.com/*
 // @grant        GM_xmlhttpRequest
